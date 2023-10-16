83893d1ea98342477240ae1d70e031dc9fe9078c
ibbinazmi03 <108617909+ibbinazmi03@users.noreply.github.com>
2023-10-16 23:05:38 +0530

    Update WelcomeController.java

--- StudentRegistrationSystem/src/main/java/com/greatlearning/srs/controller/WelcomeController.java
+++ StudentRegistrationSystem/src/main/java/com/greatlearning/srs/controller/WelcomeController.java
@@ -1,5 +1,5 @@
 package com.greatlearning.srs.controller;
-
+ 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
