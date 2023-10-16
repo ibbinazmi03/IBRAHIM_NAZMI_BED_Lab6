2d6a90d7def31a59009dfe598fe87a8368fb6396
ibbinazmi03 <108617909+ibbinazmi03@users.noreply.github.com>
2023-10-16 23:03:01 +0530

    Update SrsSecurityCustomizer.java

--- StudentRegistrationSystem/src/main/java/com/greatlearning/srs/security/SrsSecurityCustomizer.java
+++ StudentRegistrationSystem/src/main/java/com/greatlearning/srs/security/SrsSecurityCustomizer.java
@@ -1,5 +1,5 @@
 package com.greatlearning.srs.security;
-
+ 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
