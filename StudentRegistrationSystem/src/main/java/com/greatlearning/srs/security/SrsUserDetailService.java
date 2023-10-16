97f71d1473f02110e1e1add4fd1be1b4b0870509
ibbinazmi03 <108617909+ibbinazmi03@users.noreply.github.com>
2023-10-16 23:02:51 +0530

    Update SrsUserDetailService.java

--- StudentRegistrationSystem/src/main/java/com/greatlearning/srs/security/SrsUserDetailService.java
+++ StudentRegistrationSystem/src/main/java/com/greatlearning/srs/security/SrsUserDetailService.java
@@ -1,7 +1,7 @@
 package com.greatlearning.srs.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetails; 
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
